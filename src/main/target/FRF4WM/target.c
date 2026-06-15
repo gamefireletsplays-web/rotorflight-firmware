cfe8b2ffda0bdd7203bc2910f6b8d7c4d56dbee1
gamefireletsplays-web <gamefire.letsplays@gmail.com>
2026-06-15 13:03:37 +0200

    Create target.c

---
+++ src/main/target/FRF4WM/target.c
@@ -0,0 +1,20 @@
+#include <platform.h>
+#include "drivers/bus.h"
+#include "drivers/io.h"
+#include "drivers/pwm_mapping.h"
+#include "drivers/timer.h"
+#include "drivers/sensor.h"
+#include "drivers/timer_def_stm32f4xx.h"
+
+timerHardware_t timerHardware[] = {
+    DEF_TIM(TIM8, CH4,  PC9,  TIM_USE_OUTPUT_AUTO, 0, 0), // S1
+    DEF_TIM(TIM8, CH3,  PC8,  TIM_USE_OUTPUT_AUTO, 0, 0), // S2
+    DEF_TIM(TIM1, CH3N, PB15, TIM_USE_OUTPUT_AUTO, 0, 0), // S3
+    DEF_TIM(TIM1, CH1,  PA8,  TIM_USE_OUTPUT_AUTO, 0, 1), // S4
+    DEF_TIM(TIM2, CH4,  PB11, TIM_USE_OUTPUT_AUTO, 0, 0), // S5
+    DEF_TIM(TIM2, CH3,  PB10, TIM_USE_OUTPUT_AUTO, 0, 0), // S6
+    DEF_TIM(TIM3, CH4,  PB1,  TIM_USE_OUTPUT_AUTO, 0, 0), // S12 / LED
+    DEF_TIM(TIM5, CH3,  PA2,  TIM_USE_ANY,         0, 0), // SoftSerial1 TX
+};
+
+const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
